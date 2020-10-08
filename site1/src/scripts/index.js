041eeb428ce15f2a2ea19f10be11fdc1400b5bf0
SaLi <saliwork16@gmail.com>
2020-10-08 15:36:53 +0300

    0.036

--- site1/src/scripts/index.js
+++ site1/src/scripts/index.js
@@ -1,58 +1,56 @@
 (() => {
-    // slider1
-    var swiper1 = new Swiper('#swiper-container-1', {
-        direction: 'vertical',
-        slidesPerView: 1,
-        slidesPerGroup: 1,
-        effect: 'cube',
-        grabCursor: true,
-        cubeEffect: {
-            shadow: false,
-            slideShadows: false,
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-    });
+  // slider1
+  var swiper1 = new Swiper('#swiper-container-1', {
+    direction: 'vertical',
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    effect: 'cube',
+    grabCursor: true,
+    cubeEffect: {
+      shadow: false,
+      slideShadows: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
 
-    //slider2
-    var swiper2 = new Swiper('#swiper-container-2', {
-        slidesPerView: 4,
-        spaceBetween: 10,
-        slidesPerGroup: 1,
-        navigation: {
-            nextEl: '#swiper-button1',
-            prevEl: '#swiper-button2',
-        },
-    });
+  //slider2
+  var swiper2 = new Swiper('#swiper-container-2', {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    slidesPerGroup: 1,
+    navigation: {
+      nextEl: '#swiper-button1',
+      prevEl: '#swiper-button2',
+    },
+  });
 
-    //section 3
-    var swiper3 = new Swiper('#swiper-container-3', {
-        slidesPerView: 1,
-        spaceBetween: 10,
-        slidesPerGroup: 1,
-        // loop: true,
-        centeredSlidesBounds: true,
-        loopFillGroupWithBlank: true,
-        navigation: {
-            nextEl: '#swiper-button-next-4',
-            prevEl: '#swiper-button-prev-4',
-        },
-    });
+  //section 3
+  var swiper3 = new Swiper('#swiper-container-3', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    slidesPerGroup: 1,
+    // loop: true,
+    centeredSlidesBounds: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: '#swiper-button-next-4',
+      prevEl: '#swiper-button-prev-4',
+    },
+  });
 
-
-    // slider4
-    var swiper4 = new Swiper('#swiper-container-4', {
-        slidesPerView: 1,
-        spaceBetween: 100,
-        slidesPerGroup: 1,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        navigation: {
-            nextEl: '#swiper-button-next-4',
-            prevEl: '#swiper-button-prev-4',
-        },
-    });
-
+  // slider4
+  var swiper4 = new Swiper('#swiper-container-4', {
+    slidesPerView: 1,
+    spaceBetween: 100,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: '#swiper-button-next-4',
+      prevEl: '#swiper-button-prev-4',
+    },
+  });
 })();
